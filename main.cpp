bd5795a7757a9d2222292a2e11909da4f273c155
atlantisel <80100913+atlantisel@users.noreply.github.com>
2021-11-08 15:22:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,28 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include "Classes.h"
+
 
 using namespace std;
 
 int main() {
     string fileName = "Food List.csv";
     const int fieldCount = 4;
-    
     ifstream menu(fileName);
+    
     string line;
-    getline(menu, line);
-    cout << line;
-    cout << "why" ; 
-
-    return 0;
-} // wait ok lets start from the beginning
-// if i were you i would do the same 
-// i- you mean liveshare?ok yeah sure
+    while (getline(menu, line)) {
+        string field;
+        string fields[fieldCount];
+        stringstream ss(line);
+        for (int i = 0; getline(ss, field, ','); i++) {
+            fields[i] = field;
+        }
+        for (string str : fields) {
+            cout << str << " | ";
+        }
+        
+    } 
+    
+    return 0; 
+}
